9747d2aa0671afe886c186935dc80b7736b047e5
jwc20 <choij2015@gmail.com>
2023-02-17 17:29:52 +0900

    Refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-// const list = [
-//   { id: 1, title: "Complete William Vincent Django Todo App", body: "-" },
-//   { id: 2, title: "Take a lunch break.", body: "-" },
-//   { id: 3, title: "Do some third task, idk.", body: "-" },
-// ];
 
 function App() {
   const [todos, setTodos] = useState([]);
