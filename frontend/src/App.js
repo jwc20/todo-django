1c06faf389354e8a48dfa57865eba99dbe7fb6cd
jwc20 <choij2015@gmail.com>
2023-02-17 15:11:51 +0900

    Fetched data from backend api and rendered them on React app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const list = [
-  { id: 1, title: "Complete William Vincent Django Todo App", body: "-" },
-  { id: 2, title: "Take a lunch break.", body: "-" },
-  { id: 3, title: "Do some third task, idk.", body: "-" },
-];
+// const list = [
+//   { id: 1, title: "Complete William Vincent Django Todo App", body: "-" },
+//   { id: 2, title: "Take a lunch break.", body: "-" },
+//   { id: 3, title: "Do some third task, idk.", body: "-" },
+// ];
 
 function App() {
-  const [todos, setTodos] = useState(list);
+  const [todos, setTodos] = useState([]);
+  const url = "http://127.0.0.1:8000/api/";
+
+  useEffect(() => {
+    getAllTodos();
+  }, []);
+
+  const getAllTodos = async () => {
+    axios
+      .get(url)
+      .then((response) => {
+        const allTodos = response.data;
+        setTodos(allTodos);
+      })
+      .catch((error) => console.error(error));
+  };
+
+
 
   return (
     <div>
-      {list.map((item, index) => (
+      {todos.map((item, index) => (
         <div key={item.id}>
           <h1>{item.title}</h1>
           <p>{item.body}</p>
